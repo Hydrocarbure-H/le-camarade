2663370c527cb1722b62488efb7112b77fc65825
Thomas Peugnet <thomas.peugnet.dev@gmail.com>
2023-05-03 22:39:30 +0200

    Fixing answer to each message

--- resources/keywords.py
+++ resources/keywords.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 # Create a list from 2 files
 with open("resources/keywords/insults/1.txt", 'r') as f:
-    file = f.readlines()
+    file1 = f.readlines()
 with open("resources/keywords/insults/2.txt", 'r') as f:
     file2 = f.readlines()
 with open("resources/keywords/insults/3.txt", 'r') as f:
@@ -12,8 +12,11 @@ with open("resources/keywords/insults/4.txt", 'r') as f:
 with open("resources/keywords/insults/5.txt", 'r') as f:
     file5 = f.readlines()
 
-insults_keywords = [x.strip() for x in file5]
-print(insults_keywords)
+insults_keywords = [x.strip() for x in file1] + [x.strip() for x in file2] + [x.strip() for x in file5]
+# Remove duplicates and empty strings
+insults_keywords = list(dict.fromkeys(insults_keywords))
+insults_keywords = list(filter(None, insults_keywords))
+
 with open("resources/keywords/negative/1.txt", 'r') as f:
     file1 = f.readlines()
 
--- work/analyse.py
+++ work/analyse.py
@@ -48,10 +48,8 @@ def check_content(text):
     txt = text.lower()
 
     # # Check if the txt contains a insult keyword
-    for i in range(0, len(keywords[Keywords.INSULT.value])):
-        if keywords[Keywords.INSULT.value][i] in txt:
-            print("INSULT KEYWORD : " + keywords[Keywords.INSULT.value][i])
-            return Keywords.INSULT
+    if any(x in txt for x in keywords[Keywords.INSULT.value]):
+        return Keywords.INSULT
 
     # Check if the txt contains a drift keyword
     if any(x in txt for x in keywords[Keywords.DRIFT.value]):
